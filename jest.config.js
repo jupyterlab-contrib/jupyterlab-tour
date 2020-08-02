a53f85f51e1cbba93d4fe4da0b60589eb6e65cdb
Frederic Collonval <fcollonval@gmail.com>
2020-08-02 16:04:17 +0200

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+const jestJupyterLab = require('@jupyterlab/testutils/lib/jest-config');
+
+const jlabConfig = jestJupyterLab('jupyterlab-tour', __dirname);
+
+const {
+  moduleFileExtensions,
+  moduleNameMapper,
+  preset,
+  setupFilesAfterEnv,
+  setupFiles,
+  testPathIgnorePatterns,
+  transform
+} = jlabConfig;
+
+module.exports = {
+  coverageDirectory: "coverage",
+  moduleFileExtensions,
+  moduleNameMapper,
+  preset,
+  setupFilesAfterEnv,
+  setupFiles,
+  testPathIgnorePatterns,
+  transform,
+  automock: false,
+  collectCoverageFrom: ['src/**.{ts,tsx}', '!src/*.d.ts'],
+  coverageReporters: ['lcov', 'text'],
+  globals: {
+      'ts-jest': {
+          tsConfig: `./tsconfig.json`
+      }
+  },
+  reporters: ['default'],
+  testRegex: 'src/.*/.*.spec.ts[x]?$',
+  transformIgnorePatterns: ['/node_modules/(?!(@?jupyterlab.*)/)']
+};
