57f376a661d5fffd3bf4b034fb6a71e8cb4839cb
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-08-14 15:03:59 +0200

    Lint the code

--- ui-tests/tests/jupyterlab-tour.spec.ts
+++ ui-tests/tests/jupyterlab-tour.spec.ts
@@ -27,8 +27,8 @@ test('should run the notebook tour', async ({ page }) => {
   } else {
     await page.getByRole('button', { name: 'Select', exact: true }).click();
   }
-  
-  const nth = await page.getByRole('alert').count() === 2 ? 1: 0;
+
+  const nth = (await page.getByRole('alert').count()) === 2 ? 1 : 0;
   await page.getByRole('button', { name: 'Start now' }).nth(nth).click();
   await page.getByLabel('Next', { exact: true }).click();
   await page.getByLabel('Next', { exact: true }).click();
