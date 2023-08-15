48d0e0585f1c6aaf2c25d1721da0f059460c8b75
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-08-15 15:50:22 +0200

    Fix server configuration for notebook 7

--- ui-tests/jupyter_server_test_config.py
+++ ui-tests/jupyter_server_test_config.py
@@ -9,6 +9,13 @@ try:
     from jupyterlab.galata import configure_jupyter_server
 
     configure_jupyter_server(c)
+
+    # Option specific for notebook v7+
+    import jupyterlab
+    from pathlib import Path
+    c.JupyterNotebookApp.expose_app_in_browser = True
+    c.LabServerApp.extra_labextensions_path = str(Path(jupyterlab.__file__).parent / "galata")
+
 except ImportError:
     from tempfile import mkdtemp
 
